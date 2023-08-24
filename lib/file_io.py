769647dce8b4e3aa5fb63937556407b8583909f9
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-24 17:03:34 +0300

    completed assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode='w') as test:
+        test.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", mode='a') as test:
+        test.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as test:
+        return test.read()
